cf56494ce11f94ef902be5534821774b04a6ac14
Fransuelton <elton6103@gmail.com>
2026-04-26 11:49:42 -0300

    feat: implement 'define' and 'if' operations in evaluator function

--- evaluator.js
+++ evaluator.js
@@ -24,6 +24,23 @@ function evaluate(ast, env) {
   }
 
   if (Array.isArray(ast)) {
+    const op = ast[0];
+
+    if (op === "define") {
+      const name = ast[1]
+      const value = evaluate(ast[2], env);
+      env[name] = value;
+      return undefined;
+    }
+
+    if (op === "if") {
+      const value = evaluate(ast[1], env)
+      return value ? evaluate(ast[2], env) : evaluate(ast[3], env);
+    }
+    if (op === "lambda") {
+      // ...
+    }
+
     const func = evaluate(ast[0], env);
     const args = ast.slice(1).map((arg) => evaluate(arg, env));
     return func(...args);
@@ -32,14 +49,19 @@ function evaluate(ast, env) {
   throw new Error("Invalid AST");
 }
 
-console.log(evaluate(parse("(+ 1 2)"), globalEnv));
-// Esperado: 3
+// console.log(evaluate(parse("(+ 1 2)"), globalEnv));
+// // Esperado: 3
 
-console.log(evaluate(parse("(+ 1 (* 2 3))"), globalEnv));
-// Esperado: 7
+// console.log(evaluate(parse("(+ 1 (* 2 3))"), globalEnv));
+// // Esperado: 7
 
-console.log(evaluate(parse("(- 10 (/ 20 4))"), globalEnv));
-// Esperado: 5
+// console.log(evaluate(parse("(- 10 (/ 20 4))"), globalEnv));
+// // Esperado: 5
 
-console.log(evaluate(parse("(> 5 3)"), globalEnv));
-// Esperado: true
+// console.log(evaluate(parse("(> 5 3)"), globalEnv));
+// // Esperado: true
+
+console.log(evaluate(parse("(if (> 5 3) 100 200)"), globalEnv));  // 100
+console.log(evaluate(parse("(if (< 5 3) 100 200)"), globalEnv));  // 200
+evaluate(parse("(define x 10)"), globalEnv);
+console.log(evaluate(parse("(if (> x 5) (+ x 1) 0)"), globalEnv)); // 11 (x ainda vale 10)
