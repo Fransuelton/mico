5342ca6fead477d79de6db569c5c7491b15ab235
Fransuelton <elton6103@gmail.com>
2026-04-26 10:59:54 -0300

    feat: implement parsing functionality for tokenized input

--- mico.js
+++ mico.js
@@ -1,6 +1,41 @@
 import { tokenize } from "./tokenizer.js";
 
-console.log(tokenize("(+ 1 2)"));
-console.log(tokenize("(+ 1 (* 2 3))"));
-console.log(tokenize("(define x 10)"));
-console.log(tokenize("  (  + 1   2  )  "));
+function parse(source) {
+  const tokens = tokenize(source);
+  return readFromTokens(tokens);
+}
+
+function readFromTokens(tokens) {
+  const token = tokens.shift();
+
+  if (token === "(") {
+    let tokensList = [];
+
+    while (tokens[0] !== ")") {
+      tokensList.push(readFromTokens(tokens));
+    }
+
+    tokens.shift();
+    return tokensList;
+  }
+
+  if (token === ")") {
+    throw new Error("Error!");
+  } else {
+    const num = Number(token);
+    if (!isNaN(num)) return num;
+    return token;
+  }
+}
+
+console.log(JSON.stringify(parse("(+ 1 2)")));
+// Esperado: ["+",1,2]
+
+console.log(JSON.stringify(parse("(+ 1 (* 2 3))")));
+// Esperado: ["+",1,["*",2,3]]
+
+console.log(JSON.stringify(parse("(define x 10)")));
+// Esperado: ["define","x",10]
+
+console.log(JSON.stringify(parse("(if (> x 5) grande pequeno)")));
+// Esperado: ["if",[">","x",5],"grande","pequeno"]
